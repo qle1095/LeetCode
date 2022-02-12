e161b8b7708e271fac99d06e87431809eb5856d7
qle007 <qle007@fiu.edu>
2022-02-11 23:55:54 -0500

    fib

---
+++ src/leetcode/dynamicprogramming/Fib.java
@@ -0,0 +1,36 @@
+package leetcode.dynamicprogramming;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Fib {
+
+    @Test
+    public void test() {
+        System.out.println(fib(20, new HashMap<>()));
+    }
+
+
+    //time: O(2*n) space:O(n)
+    private int fib(int num) {
+        if (num <= 2) {
+            return 1;
+        }
+        return fib(num - 1) + fib(num - 2);
+    }
+    //time: O(n) space: O(n)
+    //with memoization
+    private int fib(int num, Map<Integer, Integer> memo) {
+        if (memo.containsKey(num)) {
+            return memo.get(num);
+        }
+        if (num <= 2) {
+            return 1;
+        }
+        int result = fib(num - 1, memo) + fib(num - 2, memo);
+        memo.put(num, result);
+        return result;
+    }
+}
---
+++ src/leetcode/dynamicprogramming/Grid.java
@@ -0,0 +1,9 @@
+package leetcode.dynamicprogramming;
+import org.junit.jupiter.api.Test;
+public class Grid {
+
+    @Test
+    public void test(){
+
+    }
+}
